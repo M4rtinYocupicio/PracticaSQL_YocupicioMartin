3d7d98a0e28fa1428df0e08db023b09721aa0cc6
M4rtinYocupicio <martin.yocupicio228307@potros.itson.edu.mx>
2025-06-26 09:52:08 -0700

    data class Alumno creada, así como se agregó el boton de guardar y todo lo necesario para que se guarde la información del alumno

---
+++ app/src/main/java/yocupicio/martin/practicasql_yocupiciomartin/Alumno.kt
@@ -0,0 +1,8 @@
+package yocupicio.martin.practicasql_yocupiciomartin
+
+data class Alumno(
+    val nombre: String,
+    val apPaterno: String,
+    val apMaterno: String,
+    val carrera: String
+)
--- app/src/main/java/yocupicio/martin/practicasql_yocupiciomartin/MainActivity.kt
+++ app/src/main/java/yocupicio/martin/practicasql_yocupiciomartin/MainActivity.kt
@@ -1,6 +1,8 @@
 package yocupicio.martin.practicasql_yocupiciomartin
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,24 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        // Referencias a los EditText
+        val etNombre = findViewById<EditText>(R.id.nombre)
+        val etApPat = findViewById<EditText>(R.id.apPat)
+        val etApMat = findViewById<EditText>(R.id.apMat)
+        val etCarrera = findViewById<EditText>(R.id.carrera)
+        val btnGuardar = findViewById<Button>(R.id.btnGuardar)
+
+        btnGuardar.setOnClickListener {
+            val alumno = Alumno(
+                nombre = etNombre.text.toString(),
+                apPaterno = etApPat.text.toString(),
+                apMaterno = etApMat.text.toString(),
+                carrera = etCarrera.text.toString()
+            )
+
+            println("Alumno guardado: $alumno")
         }
     }
 }
